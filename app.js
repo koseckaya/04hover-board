553d4c1700b16ef1a572b97ece1a15c380ea5a3b
interika <interika11@gmail.com>
2021-07-05 20:36:20 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 const board = document.querySelector('#board')
-const colors = ['#96f97b', '#c20078','#c79fef','#00ffff', '#ff81c0','#ffff14', 
-'#bf77f6', "#06c2ac"]
+const colors = ['#96f97b', '#c20078', '#c79fef', '#00ffff', '#ff81c0', '#ffff14',
+    '#bf77f6', "#06c2ac"]
 const SQUARES_NUMBER = 597
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
-    square.addEventListener('mouseover',  () => setColor(square))
+    square.addEventListener('mouseover', setColor)
 
-    square.addEventListener('mouseleave', () => removeColor(square))
+    square.addEventListener('mouseleave', removeColor)
 
     board.append(square)
 }
 
-function setColor(el) {
+function setColor(e) {
+    const el = e.target
     const color = getRandomColor()
     el.style.backgroundColor = color
-    el.style.boxShadow =`0 0 2px ${color}, 0 0 10px ${color}`
+    el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(el) {
+function removeColor(e) {
+    const el = e.target
     el.style.backgroundColor = '#1d1d1d'
-    el.style.boxShadow =`0 0 2px #000`
+    el.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor() {
